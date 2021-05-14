833b2913cba62ab2aaee67046611c75bd0eebc2c
askges20 <askges20@kyonggi.ac.kr>
2021-05-15 01:45:40 +0900

    일기장 페이지 레이아웃 틀 구현 (달력, 일기 내용 페이지)

--- app/src/main/java/com/hanium/android/maeumi/Diary.java
+++ app/src/main/java/com/hanium/android/maeumi/Diary.java
@@ -1,13 +1,21 @@
 package com.hanium.android.maeumi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Diary extends Activity {
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_main);
+    }
+
+    public void goToDiaryContent(View view){
+        Intent intent = new Intent(Diary.this, DiaryContent.class);
+        startActivity(intent);
+        System.out.println("Move To Diary Content");
     }
 
 }
