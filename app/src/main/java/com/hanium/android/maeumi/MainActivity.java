c8c4ed21014a42c34cc9d0a2818e08626461aa9d
askges20 <askges20@kyonggi.ac.kr>
2021-05-16 22:38:14 +0900

    firebase database test

--- app/src/main/java/com/hanium/android/maeumi/MainActivity.java
+++ app/src/main/java/com/hanium/android/maeumi/MainActivity.java
@@ -1,10 +1,17 @@
 package com.hanium.android.maeumi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Main Main");
+
+        //firebase 연동 테스트
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                System.out.println("Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                System.out.println("Failed to read value.");
+            }
+        });
     }
 
     public void goToDiary(View view){
