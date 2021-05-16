af718ad2c7a4e0b5fc0b4fe06030c3637e520a3c
askges20 <askges20@kyonggi.ac.kr>
2021-05-16 19:17:02 +0900

    일기장 수정 페이지 생성

--- app/src/main/java/com/hanium/android/maeumi/DiaryContent.java
+++ app/src/main/java/com/hanium/android/maeumi/DiaryContent.java
@@ -1,12 +1,20 @@
 package com.hanium.android.maeumi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DiaryContent extends Activity {
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_content);
+    }
+
+    public void goToDiaryModify(View view){
+        Intent intent = new Intent(DiaryContent.this, DiaryModify.class);
+        startActivity(intent);
+        System.out.println("Move To Modify Diary");
     }
 }
