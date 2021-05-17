d11adfb1c7d0600872e9d923202516e21388b755
mseob <kimaudtjq@kyonggi.ac.kr>
2021-05-17 10:36:46 +0900

    구조 수정

---
+++ app/src/main/java/com/hanium/android/maeumi/view/profile/Profile.java
@@ -0,0 +1,14 @@
+package com.hanium.android.maeumi.view.profile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hanium.android.maeumi.R;
+
+public class Profile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_main);
+    }
+}
