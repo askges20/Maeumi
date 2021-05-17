e14f1b3d885c171e4805b1dc0caa0b6a904e3a41
mseob <kimaudtjq@kyonggi.ac.kr>
2021-05-17 21:21:43 +0900

    Profile 수정화면 연결

--- app/src/main/java/com/hanium/android/maeumi/view/profile/Profile.java
+++ app/src/main/java/com/hanium/android/maeumi/view/profile/Profile.java
@@ -1,7 +1,9 @@
 package com.hanium.android.maeumi.view.profile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hanium.android.maeumi.R;
 
@@ -10,5 +12,11 @@ public class Profile extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_main);
+    }
+
+    public void goToProfileEdit(View view){
+        Intent intent = new Intent(Profile.this,ProfileEdit.class);
+        startActivity(intent);
+        System.out.println("Move To Profile Edit");
     }
 }
---
+++ app/src/main/java/com/hanium/android/maeumi/view/profile/ProfileEdit.java
@@ -0,0 +1,16 @@
+package com.hanium.android.maeumi.view.profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hanium.android.maeumi.R;
+
+public class ProfileEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit);
+    }
+}
