ca1def28f08924ff2267ac63307fc761661d9ca6
askges20 <askges20@kyonggi.ac.kr>
2021-06-25 22:23:33 +0900

    자동정렬, DiaryMiddleViewModel <- view에 w가 빠져 있어서 수정

--- app/src/main/java/com/hanium/android/maeumi/view/diary/CustomAction.java
+++ app/src/main/java/com/hanium/android/maeumi/view/diary/CustomAction.java
@@ -23,7 +23,7 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
     private RecyclerView calendarRecyclerView;
     private LocalDate selectDate;
 
-    DiaryMiddleViewModel DiaryMiddleVieModel = new DiaryMiddleViewModel();
+    DiaryMiddleViewModel DiaryMiddleViewModel = new DiaryMiddleViewModel();
 
 
     @Override
@@ -32,10 +32,10 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
         setContentView(R.layout.activity_custom);
         iniwigets();
         selectDate = LocalDate.now();
-        System.out.println("selectDate : "+selectDate);
+        System.out.println("selectDate : " + selectDate);
         setMonthView();
         // 월별 일기 조회 후 점 찍기
-        DiaryMiddleVieModel.setMaxDay(selectDate);
+        DiaryMiddleViewModel.setMaxDay(selectDate);
     }
 
 
@@ -53,7 +53,7 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
         monthYearText.setText(monthYearFromDate(selectDate));
         ArrayList<String> daysInMonth = daysInMonthArray(selectDate);
 
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth,this);
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
@@ -62,14 +62,14 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
     private ArrayList<String> daysInMonthArray(LocalDate date) {
         ArrayList<String> dayInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(date);
-        System.out.println("yearMonth : "+yearMonth);
+        System.out.println("yearMonth : " + yearMonth);
 
         int daysInMonth = yearMonth.lengthOfMonth();
 
         LocalDate first0fMonth = selectDate.withDayOfMonth(1);
-        System.out.println("firstOfMonth : "+ first0fMonth);
+        System.out.println("firstOfMonth : " + first0fMonth);
         int day0fweek = first0fMonth.getDayOfWeek().getValue();
-        System.out.println("dayOfweek"+day0fweek);
+        System.out.println("dayOfweek" + day0fweek);
 
         for (int i = 1; i <= 42; i++) {
             if (i <= day0fweek || i > daysInMonth + day0fweek) {
@@ -82,40 +82,41 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
     }
 
 
-        public void previousMonthAction (View view){
-            selectDate = selectDate.minusMonths(1);
-            // 월별 일기 조회 후 점 찍기
-            System.out.println("previous Month - "+ selectDate);
-            DiaryMiddleVieModel.setMaxDay(selectDate);
-            setMonthView();
-        }
+    public void previousMonthAction(View view) {
+        selectDate = selectDate.minusMonths(1);
+        // 월별 일기 조회 후 점 찍기
+        System.out.println("previous Month - " + selectDate);
+        DiaryMiddleViewModel.setMaxDay(selectDate);
+        setMonthView();
+    }
 
-        public void nextMonthAction (View view){
-            selectDate = selectDate.plusMonths(1);
-            // 월별 일기 조회 후 점 찍기
-            System.out.println("next Month- "+ selectDate);
-            DiaryMiddleVieModel.setMaxDay(selectDate);
-            setMonthView();
-        }
+    public void nextMonthAction(View view) {
+        selectDate = selectDate.plusMonths(1);
+        // 월별 일기 조회 후 점 찍기
+        System.out.println("next Month- " + selectDate);
+        DiaryMiddleViewModel.setMaxDay(selectDate);
+        setMonthView();
+    }
 
-        @Override
-        public void onItemClick ( int position, String dayText){
-            // 클릭 날짜 표시 [x]
-            // ViewModel에 날짜 저장 [o]
-        DiaryMiddleVieModel.setDate(selectDate,dayText);
-                String message = "Selected Date" + dayText + "" + monthYearFromDate(selectDate);
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-        }
-    public void diaryContent(View view){
+    @Override
+    public void onItemClick(int position, String dayText) {
+        // 클릭 날짜 표시 [x]
+        // ViewModel에 날짜 저장 [o]
+        DiaryMiddleViewModel.setDate(selectDate, dayText);
+        String message = "Selected Date" + dayText + "" + monthYearFromDate(selectDate);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void diaryContent(View view) {
         Intent intent = new Intent(getApplicationContext(), DiaryContent.class);
         startActivity(intent);
         System.out.println("Move To Diary Content");
     }
 
-    public void diaryWrite(View view){
-        Intent intent = new Intent(getApplicationContext(),DiaryWrite.class);
+    public void diaryWrite(View view) {
+        Intent intent = new Intent(getApplicationContext(), DiaryWrite.class);
         startActivity(intent);
         System.out.println("Move To Diary Write");
     }
-    }
+}
 
