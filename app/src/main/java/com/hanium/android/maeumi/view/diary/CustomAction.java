161c46e87e30ade6a8a79f3ceadf49d317eb519d
askges20 <askges20@kyonggi.ac.kr>
2021-06-25 22:45:34 +0900

    커스텀 캘린더 관련 메소드 주석 추가

--- app/src/main/java/com/hanium/android/maeumi/view/diary/CustomAction.java
+++ app/src/main/java/com/hanium/android/maeumi/view/diary/CustomAction.java
@@ -38,27 +38,31 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
         DiaryMiddleViewModel.setMaxDay(selectDate);
     }
 
-
+    //activity_custom.xml 레이아웃 요소 연결
     private void iniwigets() {
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
         monthYearText = findViewById(R.id.monthYearTV);
     }
 
+    //캘린더 상단 년,월 (m월 yyyy)
     private String monthYearFromDate(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         return date.format(formatter);
     }
 
+    //캘린더 새로고침
     private void setMonthView() {
         monthYearText.setText(monthYearFromDate(selectDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectDate);
+        ArrayList<String> daysInMonth = daysInMonthArray(selectDate);   //선택한 날짜의 일들 구하기
 
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
     }
 
+    //선택한 날짜의 월 구하기
     private ArrayList<String> daysInMonthArray(LocalDate date) {
         ArrayList<String> dayInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(date);
@@ -66,13 +70,13 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
 
         int daysInMonth = yearMonth.lengthOfMonth();
 
-        LocalDate first0fMonth = selectDate.withDayOfMonth(1);
+        LocalDate first0fMonth = selectDate.withDayOfMonth(1);  //해당 월의 첫번째 일
         System.out.println("firstOfMonth : " + first0fMonth);
-        int day0fweek = first0fMonth.getDayOfWeek().getValue();
+        int day0fweek = first0fMonth.getDayOfWeek().getValue();     //첫번째 일 (숫자)
         System.out.println("dayOfweek" + day0fweek);
 
         for (int i = 1; i <= 42; i++) {
-            if (i <= day0fweek || i > daysInMonth + day0fweek) {
+            if (i <= day0fweek || i > daysInMonth + day0fweek) {    //달력 범위 벗어나는 부분 빈문자열 처리
                 dayInMonthArray.add("");
             } else {
                 dayInMonthArray.add(String.valueOf(i - day0fweek));
@@ -81,7 +85,7 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
         return dayInMonthArray;
     }
 
-
+    //이전 달 캘린더로 이동
     public void previousMonthAction(View view) {
         selectDate = selectDate.minusMonths(1);
         // 월별 일기 조회 후 점 찍기
@@ -90,6 +94,7 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
         setMonthView();
     }
 
+    //다음 달 캘린더로 이동
     public void nextMonthAction(View view) {
         selectDate = selectDate.plusMonths(1);
         // 월별 일기 조회 후 점 찍기
@@ -98,21 +103,25 @@ public class CustomAction extends AppCompatActivity implements CalendarAdapter.O
         setMonthView();
     }
 
+    //캘린더에서 날짜 선택했을 때 이벤트 리스너
     @Override
     public void onItemClick(int position, String dayText) {
         // 클릭 날짜 표시 [x]
         // ViewModel에 날짜 저장 [o]
         DiaryMiddleViewModel.setDate(selectDate, dayText);
-        String message = "Selected Date" + dayText + "" + monthYearFromDate(selectDate);
+        String message = "Selected Date" + dayText + " " + monthYearFromDate(selectDate);
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
+    //일기 내용 페이지로 이동
+    //추후 캘린더에 일기 작성 여부에 따른 점 표시 구현 -> (일기가 있는 날이면) 날짜 선택 시 자동으로 일기 내용 페이지로 이동시킬 예정
     public void diaryContent(View view) {
         Intent intent = new Intent(getApplicationContext(), DiaryContent.class);
         startActivity(intent);
         System.out.println("Move To Diary Content");
     }
 
+    //일기 작성 페이지로 이동
     public void diaryWrite(View view) {
         Intent intent = new Intent(getApplicationContext(), DiaryWrite.class);
         startActivity(intent);
