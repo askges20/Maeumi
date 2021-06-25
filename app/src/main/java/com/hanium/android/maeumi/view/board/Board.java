89ace87cce42fb68b6081e0c5b3b2a4e2a6157fd
askges20 <askges20@kyonggi.ac.kr>
2021-06-26 00:16:55 +0900

    게시판 Fragment화 기존에는 Intent로 페이지 이동, 현재는 Fragment로 한 화면에서 자유/익명게시판 이동

--- app/src/main/java/com/hanium/android/maeumi/view/board/Board.java
+++ app/src/main/java/com/hanium/android/maeumi/view/board/Board.java
@@ -5,65 +5,105 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.hanium.android.maeumi.R;
-import com.hanium.android.maeumi.model.Post;
-import com.hanium.android.maeumi.viewmodel.PostAdapter;
 
 public class Board extends AppCompatActivity {
+    private final int fragment_1 = 1; //자유게시판
+    private final int fragment_2 = 2; //익명게시판
+    private int cur_fragment;   //현재 fragment 번호
+
+    TextView boardNameText;
+
     @Override
-    protected  void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_main);
+        boardNameText = findViewById(R.id.boardNameText);
 
-        RecyclerView recyclerView = findViewById(R.id.boardRecyclerView);
+        //게시판 버튼 이벤트 리스너 추가
+        findViewById(R.id.freeBoardBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentView(fragment_1);
+            }
+        });
 
-        //레이아웃 매니저로 배치 방법 설정
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
+        findViewById(R.id.anonymousBoardBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentView(fragment_2);
+            }
+        });
 
-        //어댑터를 이용해서 리사이클러뷰에 데이터 넘김
-        //추후 firebase 데이터와 연결할 것
-        PostAdapter adapter = new PostAdapter();
-        for (int i=0;i<10;i++)
-            adapter.addItem(new Post("제목입니다","작성자입니다","작성일자"));
-        recyclerView.setAdapter(adapter);
+        cur_fragment = fragment_1;
+        FragmentView(cur_fragment);   //처음 화면은 자유게시판
+        boardNameText.setText("자유게시판");
     }
 
+    //fragment 부착
+    private void FragmentView(int fragment) {
+        //FragmentTransactiom를 이용해 fragment 사용
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragment) {
+            case 1:
+                // 첫번째 fragment(자유게시판) 호출
+                BoardFreeFragment freeFragment = new BoardFreeFragment();
+                transaction.replace(R.id.board_fragment_container, freeFragment);
+                transaction.commit();
+                cur_fragment = fragment_1;  //현재 fragment 번호 업데이트
+                boardNameText.setText("자유게시판");
+                break;
+
+            case 2:
+                // 두번째 fragment(익명게시판) 호출
+                BoardAnonymousFragment anonymousFragment = new BoardAnonymousFragment();
+                transaction.replace(R.id.board_fragment_container, anonymousFragment);
+                transaction.commit();
+                cur_fragment = fragment_2; //현재 fragment 번호 업데이트
+                boardNameText.setText("익명게시판");
+                break;
+        }
+    }
+
+    /*
     public void goToBoardAnonymous(View view){
         Intent intent = new Intent(Board.this, BoardAnonymous.class);
         startActivity(intent);
         System.out.println("익명게시판으로 이동");
     }
+    */
 
-    public void goToBoardWrite(View view){
+    public void goToBoardWrite(View view) {
         Intent intent = new Intent(Board.this, BoardWrite.class);
         startActivity(intent);
         System.out.println("자유게시판 게시글 작성");
     }
 
+    //정렬 클릭
     public void onFilterClick(final View view) {
-            final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
-            getMenuInflater().inflate(R.menu.board_popup_menu,popupMenu.getMenu());
-            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem menuItem) {
-                    if (menuItem.getItemId() == R.id.action_menu1){
-                        Toast.makeText(Board.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
-                    }else if (menuItem.getItemId() == R.id.action_menu2){
-                        Toast.makeText(Board.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
-                    }else {
-                        Toast.makeText(Board.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
-                    }
-
-                    return false;
+        final PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+        getMenuInflater().inflate(R.menu.board_popup_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.action_menu1) {
+                    Toast.makeText(Board.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
+                } else if (menuItem.getItemId() == R.id.action_menu2) {
+                    Toast.makeText(Board.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Board.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
                 }
-            });
-            popupMenu.show();
-        }
+
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
 }
---
+++ app/src/main/java/com/hanium/android/maeumi/view/board/BoardFreeFragment.java
@@ -0,0 +1,52 @@
+package com.hanium.android.maeumi.view.board;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hanium.android.maeumi.R;
+import com.hanium.android.maeumi.model.Post;
+import com.hanium.android.maeumi.viewmodel.PostAdapter;
+
+/*자유게시판 fragment*/
+public class BoardFreeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //레이아웃 연결
+        View view = inflater.inflate(R.layout.board_free_fragment, container, false);
+        Context context = view.getContext();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.boardFreeRecyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        //레이아웃 매니저로 배치 방법 설정
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //어댑터를 이용해서 리사이클러뷰에 데이터 넘김
+        //추후 firebase 데이터와 연결할 것
+        PostAdapter adapter = new PostAdapter();
+        for (int i = 0; i < 10; i++)
+            adapter.addItem(new Post("제목:자유게시판", "작성자", "작성일자"));
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
