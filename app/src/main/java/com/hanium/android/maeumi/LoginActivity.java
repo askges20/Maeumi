80f7312d7dfa4ba6bc1a80285782adaf6a67eef5
mseob <kimaudtjq@kyonggi.ac.kr>
2021-07-07 23:04:54 +0900

    로그인, 회원가입 구현

--- app/src/main/java/com/hanium/android/maeumi/LoginActivity.java
+++ app/src/main/java/com/hanium/android/maeumi/LoginActivity.java
@@ -3,25 +3,79 @@ package com.hanium.android.maeumi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    FirebaseAuth firebaseAuth;
+    private String userEmail, userPassword;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.userEmail);
+        password = findViewById(R.id.userPassword);
     }
 
     public void onLoginClick(View view){
-        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-        startActivity(intent);
+        userEmail = email.getText().toString();
+        userPassword = password.getText().toString();
+
+        if (userEmail.equals("") || userPassword.equals("")){
+            Toast.makeText(LoginActivity.this,"이메일과 비밀번호를 입력하세요.",Toast.LENGTH_LONG).show();
+        }else{
+            login(userEmail,userPassword);
+
+        }
+
+        // 뭐 하는지 모르는데 예제에 있어서 추가했어요
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
     }
 
     public void onSignUpClick(View view){
         Intent intent = new Intent(LoginActivity.this,SingUpActivity.class);
         startActivity(intent);
+    }
+
+    public void login(String email, String password){
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,"로그인 성공",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this,"아이디 혹은 비밀번호가 일치하지 않습니다.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 }
--- app/src/main/java/com/hanium/android/maeumi/SingUpActivity.java
+++ app/src/main/java/com/hanium/android/maeumi/SingUpActivity.java
@@ -3,19 +3,66 @@ package com.hanium.android.maeumi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SingUpActivity extends AppCompatActivity {
+
+    EditText email, password, name;
+    FirebaseAuth firebaseAuth;
+    private String userEmail, userPassword, userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
+        email = findViewById(R.id.userEmail);
+        password = findViewById(R.id.userPassword);
+        name = findViewById(R.id.userName);
+
     }
     public void onSignUpClick(View view){
-        Intent intent = new Intent(SingUpActivity.this,LoginActivity.class);
-        startActivity(intent);
+
+        userEmail = email.getText().toString();
+        userPassword = password.getText().toString();
+        userName = name.getText().toString();
+
+        if (userEmail.equals("") || userPassword.equals("") || userName.equals("")){
+            Toast.makeText(SingUpActivity.this,"빈 칸을 채워주세요.",Toast.LENGTH_LONG).show();
+        }else{
+            createUser(userEmail,userPassword,userName);
+
+        }
+    }
+
+    private void createUser(String email, String password, String name) {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener( SingUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공시
+                            Toast.makeText(SingUpActivity.this,"회원가입 성공",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(SingUpActivity.this,LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // 계정이 중복된 경우
+                            Toast.makeText(SingUpActivity.this,"동일한 아이디가 존재합니다.",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
     }
 
 }
