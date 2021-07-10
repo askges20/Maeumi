dd4471e5489ae9c9566f17ed5c73aa400532792b
askges20 <askges20@kyonggi.ac.kr>
2021-07-11 01:29:42 +0900

    마이페이지 사용자 정보 조회 (DB)

--- app/src/main/java/com/hanium/android/maeumi/view/profile/Profile.java
+++ app/src/main/java/com/hanium/android/maeumi/view/profile/Profile.java
@@ -6,16 +6,52 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.hanium.android.maeumi.R;
-import com.hanium.android.maeumi.view.diary.DiaryContent;
 
 public class Profile extends Activity {
+
+    FirebaseDatabase database;
+    DatabaseReference userRef;
+
+    String loginUserUid;
+    TextView loginUserNameText;
+    TextView loginUserEmailText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_main);
+
+        loginUserNameText = findViewById(R.id.loginUserNameText);
+        loginUserEmailText = findViewById(R.id.loginUserEmailText);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();    //현재 로그인한 사용자
+        loginUserUid = user.getUid();   //사용자 Uid
+
+        database = FirebaseDatabase.getInstance();
+        userRef = database.getReference("/Users/" + loginUserUid + "/");
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                loginUserNameText.setText("이름 : " + snapshot.child("name").getValue(String.class)); //사용자 이름 표시
+                loginUserEmailText.setText("이메일 : " + snapshot.child("email").getValue(String.class));  //사용자 이메일 표시
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
     }
 
     public void goToProfileEdit(View view){
