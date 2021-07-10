60ff83eaeba3abbd4e0e79bf2f99a3ea7ea80067
askges20 <askges20@kyonggi.ac.kr>
2021-07-10 22:59:20 +0900

    Dialogflow 연결하는 중 dependencies, 인터페이스, 어댑터, 헬퍼 등 추가

---
+++ app/src/main/java/com/hanium/android/maeumi/interfaces/BotReply.java
@@ -0,0 +1,6 @@
+package com.hanium.android.maeumi.interfaces;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+
+public interface BotReply {
+    void callback(DetectIntentResponse returnResponse);
+}
