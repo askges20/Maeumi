e4ccdaa64093abe83ae889e0a8f871e4395b2207
mseob <kimaudtjq@kyonggi.ac.kr>
2021-07-13 20:54:24 +0900

    이메일, 비밀번호 유효성 검사(이메일 형식 확인 , 비밀번호 8자 이상, 숫자, 특수문자 각각 1개 이상)

--- app/src/main/java/com/hanium/android/maeumi/SingUpActivity.java
+++ app/src/main/java/com/hanium/android/maeumi/SingUpActivity.java
@@ -2,13 +2,12 @@ package com.hanium.android.maeumi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,12 +20,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class SingUpActivity extends AppCompatActivity {
 
-    EditText email, password, name,alias,school;
+    EditText email, password, name, alias, school;
     FirebaseAuth firebaseAuth;
-    private String userEmail, userPassword, userName, userGender,userAlias,userSchool;
+    private String userEmail, userPassword, userName, userGender, userAlias, userSchool;
     private RadioGroup radioGroup;
 
     @Override
@@ -44,9 +44,9 @@ public class SingUpActivity extends AppCompatActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == R.id.girl){
+                if (checkedId == R.id.girl) {
                     userGender = "여";
-                }else if(checkedId == R.id.man){
+                } else if (checkedId == R.id.man) {
                     userGender = "남";
                 }
             }
@@ -62,24 +62,26 @@ public class SingUpActivity extends AppCompatActivity {
         userAlias = alias.getText().toString();
         userSchool = school.getText().toString();
 
-        if (userEmail.equals("") || userPassword.equals("") || userName.equals("")) {
-            Toast.makeText(SingUpActivity.this, "빈 칸을 채워주세요.", Toast.LENGTH_LONG).show();
-        } else if (userPassword.length() < 6) {
-            Toast.makeText(SingUpActivity.this, "비밀번호는 6자 이상으로 입력해주세요!", Toast.LENGTH_LONG).show();
-            //이 외에도 문자, 숫자, 특수기호 포함했는지 유효성 검사 추가 필요
-        } else if(userGender == null) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        if (!pattern.matcher(userEmail).matches()) {
+            Toast.makeText(SingUpActivity.this, "이메일 형식을 맞춰주세요", Toast.LENGTH_LONG).show();
+        } else if (!Pattern.matches("^(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$", userPassword)) {
+            Toast.makeText(SingUpActivity.this, "8자 이상, 숫자, 특수문자가 각각 1개이상 입력해주세요.", Toast.LENGTH_LONG).show();
+        }else if (userName.equals("")) {
+            Toast.makeText(SingUpActivity.this, "이름을 입력해주세요.", Toast.LENGTH_LONG).show();
+        } else if (userGender == null) {
             Toast.makeText(SingUpActivity.this, "성별을 선택해주세요.", Toast.LENGTH_LONG).show();
-        }else if(userAlias == null){
+        } else if (userAlias == null) {
             Toast.makeText(SingUpActivity.this, "닉네임을 입력해주세요.", Toast.LENGTH_LONG).show();
-        }else if(userSchool == null){
+        } else if (userSchool == null) {
             Toast.makeText(SingUpActivity.this, "학교를 입력해주세요.", Toast.LENGTH_LONG).show();
-        }else{
-            createUser(userEmail, userPassword, userName,userGender,userAlias,userSchool);
+        } else {
+            createUser(userEmail, userPassword, userName, userGender, userAlias, userSchool);
 
         }
     }
-
-    private void createUser(String email, String password, String userName,String UserGender,String userAlias,String userSchool) {
+    private void createUser(String email, String password, String userName, String UserGender, String userAlias, String userSchool) {
         firebaseAuth = FirebaseAuth.getInstance();
 
         firebaseAuth.createUserWithEmailAndPassword(email, password)
@@ -100,9 +102,9 @@ public class SingUpActivity extends AppCompatActivity {
                             hashMap.put("uid", uid);
                             hashMap.put("email", email);
                             hashMap.put("name", name);
-                            hashMap.put("gender",gender);
-                            hashMap.put("alias",alias);
-                            hashMap.put("school",school);
+                            hashMap.put("gender", gender);
+                            hashMap.put("alias", alias);
+                            hashMap.put("school", school);
 
                             FirebaseDatabase database = FirebaseDatabase.getInstance();
                             DatabaseReference reference = database.getReference("Users");
