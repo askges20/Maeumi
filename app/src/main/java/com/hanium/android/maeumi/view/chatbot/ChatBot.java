3baa228de9c6a0b8b6f97f41de6222046c5c1242
Yewon <askges20@kyonggi.ac.kr>
2021-07-13 18:44:43 +0900

    1:1 상담 채팅 내역 DB 저장 구현

--- app/src/main/java/com/hanium/android/maeumi/view/chatbot/ChatBot.java
+++ app/src/main/java/com/hanium/android/maeumi/view/chatbot/ChatBot.java
@@ -21,6 +21,10 @@ import com.google.cloud.dialogflow.v2.SessionsClient;
 import com.google.cloud.dialogflow.v2.SessionsSettings;
 import com.google.cloud.dialogflow.v2.TextInput;
 import com.google.common.collect.Lists;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hanium.android.maeumi.LoginUser;
 import com.hanium.android.maeumi.R;
 import com.hanium.android.maeumi.adapters.ChatAdapter;
 import com.hanium.android.maeumi.helpers.SendMessageInBg;
@@ -28,12 +32,18 @@ import com.hanium.android.maeumi.interfaces.BotReply;
 import com.hanium.android.maeumi.model.Message;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 public class ChatBot extends AppCompatActivity implements BotReply {
+
+    FirebaseDatabase firebaseDatabase;
+    String refPath;
+    DatabaseReference chatRef;
 
     RecyclerView chatView;
     ChatAdapter chatAdapter;
@@ -64,12 +74,14 @@ public class ChatBot extends AppCompatActivity implements BotReply {
             public void onClick(View view) {
                 String message = editMessage.getText().toString();  //입력된 문장
                 if (!message.isEmpty()) {
-                    messageList.add(new Message(message, false));   //채팅 내역에 추가
+                    messageList.add(new Message(message, false));   //전송한 채팅을 채팅 내역에 추가
                     editMessage.setText("");    //채팅 입력칸 비우기
                     sendMessageToBot(message);  //채팅 전송
                     Objects.requireNonNull(chatView.getAdapter()).notifyDataSetChanged();
                     Objects.requireNonNull(chatView.getLayoutManager())
                             .scrollToPosition(messageList.size() - 1);
+
+                    addChat2FirebaseDB(true, message);  //DB에 전송한 채팅 저장하기
                 } else {    //입력 문장이 없으면
                     Toast.makeText(ChatBot.this, "Please enter text!", Toast.LENGTH_SHORT).show();
                 }
@@ -77,6 +89,10 @@ public class ChatBot extends AppCompatActivity implements BotReply {
         });
 
         setUpBot(); //Dialogflow Bot 세팅
+
+        //Firebase
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        refPath = "/채팅/" + LoginUser.getInstance().getUid() + "/";  //DB 경로
     }
 
     private void setUpBot() {
@@ -109,14 +125,35 @@ public class ChatBot extends AppCompatActivity implements BotReply {
         if (returnResponse != null) {
             String botReply = returnResponse.getQueryResult().getFulfillmentText();
             if (!botReply.isEmpty()) {
-                messageList.add(new Message(botReply, true));
+                messageList.add(new Message(botReply, true));   //챗봇 답변을 채팅 내역 ArrayList에 추가
                 chatAdapter.notifyDataSetChanged();
                 Objects.requireNonNull(chatView.getLayoutManager()).scrollToPosition(messageList.size() - 1);
+
+                addChat2FirebaseDB(false, botReply);    //DB에 botreply 저장하기
             } else {
                 Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show();
             }
         } else {
             Toast.makeText(this, "failed to connect!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    //전송한 채팅을 DB에 저장하기
+    public void addChat2FirebaseDB(boolean isUserSent, String message) {
+        Date today = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyyMMdd");
+        String dateStr = dateFormat.format(today);
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HHmmss");
+        String timeStr = timeFormat.format(today);
+
+        String who;
+        if (isUserSent){
+            who = "user";
+        } else {
+            who = "bot";
+        }
+
+        chatRef = firebaseDatabase.getReference(refPath + dateStr + "/" + timeStr + who);
+        chatRef.setValue(message);
     }
 }
