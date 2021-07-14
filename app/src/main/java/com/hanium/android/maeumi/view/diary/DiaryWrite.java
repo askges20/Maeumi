cb05dfd696c4122c33fd31a74ffd23ffb3e066b1
mseob <kimaudtjq@kyonggi.ac.kr>
2021-07-14 22:40:30 +0900

    일기 작성 시 사진 불러오기

--- app/src/main/java/com/hanium/android/maeumi/view/diary/DiaryWrite.java
+++ app/src/main/java/com/hanium/android/maeumi/view/diary/DiaryWrite.java
@@ -1,20 +1,34 @@
 package com.hanium.android.maeumi.view.diary;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.hanium.android.maeumi.R;
-import com.hanium.android.maeumi.view.board.Board;
 import com.hanium.android.maeumi.viewmodel.DiaryViewModel;
+
+import java.io.InputStream;
 
 public class DiaryWrite extends AppCompatActivity {
 
@@ -24,6 +38,7 @@ public class DiaryWrite extends AppCompatActivity {
     EditText titleText,contentText;
     String diaryCalDate, diaryEmoticon;
     LinearLayout mainContent;
+    ImageView testImgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -38,6 +53,7 @@ public class DiaryWrite extends AppCompatActivity {
         mainContent = findViewById(R.id.mainContent);
         titleText = (EditText)findViewById(R.id.diaryTitleWriteText);
         contentText = (EditText)findViewById(R.id.diaryContentWriteText);
+        testImgView = findViewById(R.id.testImgView);
         dateText.setText(diaryCalDate);
     }
     // 빈곳 클릭 시 이벤트
@@ -95,4 +111,74 @@ public class DiaryWrite extends AppCompatActivity {
         });
         popupMenu.show();
     }
+
+    public void addImg(View view){   // 사진추가 버튼 클릭 시
+        checkSelfPermission();
+
+        Intent intent = new Intent();
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,101);
+    }
+
+    public void checkSelfPermission() {
+        String temp = "";
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+
+        // 권한 요청
+        if (TextUtils.isEmpty(temp) == false) {
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+        } else { // 모두 허용 상태
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // 앨범에서 이미지 클릭하면 testImgView에 이미지 담아주기
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // 갤러리
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream inStream = resolver.openInputStream(fileUri);
+                    Bitmap imgName = BitmapFactory.decodeStream(inStream);
+                    String helloTest = imgName.toString();
+                    System.out.println("hello Test - "+ helloTest);
+                    testImgView.setImageBitmap(imgName);    // 선택한 이미지 이미지뷰에 셋
+                    inStream.close();   // 스트림 닫아주기
+//                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 성공", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // 권한 확인
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //권한을 허용 했을 경우
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                // 동의
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    System.out.println("TestImg 권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+
+    }
+
 }
