55ac3c7f0bdd569c06addfa6118b567531b4056f
mseob <kimaudtjq@kyonggi.ac.kr>
2021-08-01 21:49:55 +0900

    테스트 결과 숫자 대신 정도로 표시하기

--- app/src/main/java/com/hanium/android/maeumi/view/selftest/TestResult.java
+++ app/src/main/java/com/hanium/android/maeumi/view/selftest/TestResult.java
@@ -22,28 +22,26 @@ public class TestResult extends AppCompatActivity {
     ProgressBar victimProgress;
     ProgressBar perpetrationProgress;
 
-    TextView victimResultDetailText;
-    TextView perpetrationResultDetailText;
+    TextView victimResultDetailText,victimScoreText;
+    TextView perpetrationResultDetailText,perpetrationScoreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_result);
 
-        TextView victimScoreText = findViewById(R.id.victimScore);
-        TextView perpetrationScoreText = findViewById(R.id.perpetrationScore);
+        victimScoreText = findViewById(R.id.victimScore);
+        perpetrationScoreText = findViewById(R.id.perpetrationScore);
 
         victimScore = getIntent().getIntExtra("victim", 0);
         if (victimScore == 0) {
             victimScore = Integer.parseInt(LoginUser.getInstance().getVictimScore());
         }
-        victimScoreText.setText("" + victimScore);
 
         perpetrationScore = getIntent().getIntExtra("perpetration", 0);
         if (perpetrationScore == 0){
             perpetrationScore = Integer.parseInt(LoginUser.getInstance().getPerpetrationScore());
         }
-        perpetrationScoreText.setText("" + perpetrationScore);
 
 
         //프로그레스바 나타내기
@@ -68,27 +66,37 @@ public class TestResult extends AppCompatActivity {
         //피해 정도 결과
         if (victimScore <= 6){  //0~6점 아주 약함
             victimResultDetailText.setText(R.string.test_result_victim_1);
+            victimScoreText.setText(" 아주 약함");
         } else if (victimScore <= 13){  //7~13점 약함
             victimResultDetailText.setText(R.string.test_result_victim_2);
+            victimScoreText.setText(" 약함");
         } else if (victimScore <= 22){  //14~22점 보통
             victimResultDetailText.setText(R.string.test_result_victim_3);
+            victimScoreText.setText(" 보통");
         } else if (victimScore <= 29){  //23~29점 심함
             victimResultDetailText.setText(R.string.test_result_victim_4);
+            victimScoreText.setText(" 심함");
         } else {    //30~36점 아주 심함
             victimResultDetailText.setText(R.string.test_result_victim_5);
+            victimScoreText.setText(" 아주 심함");
         }
 
         //가해 정도 결과
         if (perpetrationScore <= 6){
             perpetrationResultDetailText.setText(R.string.test_result_perpetration_1);
+            perpetrationScoreText.setText("아주 약함");
         } else if (perpetrationScore <= 13){
             perpetrationResultDetailText.setText(R.string.test_result_perpetration_2);
+            perpetrationScoreText.setText("약함");
         } else if (perpetrationScore <= 22){
             perpetrationResultDetailText.setText(R.string.test_result_perpetration_3);
+
         } else if (perpetrationScore <= 29){
             perpetrationResultDetailText.setText(R.string.test_result_perpetration_4);
+            perpetrationScoreText.setText("심함");
         } else {
             perpetrationResultDetailText.setText(R.string.test_result_perpetration_5);
+            perpetrationScoreText.setText("아주 심함");
         }
     }
 
