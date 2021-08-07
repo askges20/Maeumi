179c7007695ac740324a5e7658af329e0d2bf838
eunhye <71172389+eunhye0606@users.noreply.github.com>
2021-08-07 15:03:28 +0900

    youtube Api 추가/ layout 수정예정중..

--- app/src/main/java/com/hanium/android/maeumi/view/heartprogram/HeartProgram.java
+++ app/src/main/java/com/hanium/android/maeumi/view/heartprogram/HeartProgram.java
@@ -1,18 +1,47 @@
 package com.hanium.android.maeumi.view.heartprogram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.hanium.android.maeumi.R;
 
 public class HeartProgram extends AppCompatActivity {
+    YouTubePlayerView youTubePlayerView;
+    Button btn;
+    YouTubePlayer.OnInitializedListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_program);
+
+        btn = findViewById(R.id.youtubeViewPlayer);
+        youTubePlayerView = findViewById(R.id.youtubeView);
+        listener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("CbGpbuOhtVE"); //동영상
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize("AIzaSyAd7jxBYoyffM5fWPST32ZYddSlbAtix48",listener); //api 키
+            }
+        });
+        
     }
 
     public void goToBack(View view) {   //뒤로가기 버튼 클릭 시
