65a1c30080aa39bb9aa8732bbe2529eda655f02c
mseob <kimaudtjq@kyonggi.ac.kr>
2021-08-13 16:20:39 +0900

    게시판 이미지 삭제

--- app/src/main/java/com/hanium/android/maeumi/view/board/PostContent.java
+++ app/src/main/java/com/hanium/android/maeumi/view/board/PostContent.java
@@ -20,10 +20,13 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -221,6 +224,8 @@ public class PostContent extends AppCompatActivity {
                         commentRef = database.getReference("/댓글/" + postCode);
                         commentRef.setValue(null);
 
+                        //DB에서 해당 게시글 이미지 삭제
+                        deleteImg();
 
                         //DB에서 해당 게시글 공감 삭제
                         DatabaseReference likeRef = database.getReference("/공감/" + postCode);
@@ -381,5 +386,15 @@ public class PostContent extends AppCompatActivity {
                 Glide.with(getApplicationContext()).load(uri).into(boardImgView);
             }
         });
+    }
+
+    private void deleteImg(){
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference("/board");
+
+        String date = writeDate.substring(0, 4) + writeDate.substring(5, 7) + writeDate.substring(8, 10);
+        String path = date + postCode;
+
+        storageRef.child(boardType).child(path).delete();
     }
 }
