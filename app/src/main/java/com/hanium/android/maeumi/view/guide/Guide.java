ffda94f3edae8376d25e1faa8143ad42ab549ca9
eunhye <71172389+eunhye0606@users.noreply.github.com>
2021-08-18 15:29:11 +0900

    guide button 생성

--- app/src/main/java/com/hanium/android/maeumi/view/guide/Guide.java
+++ app/src/main/java/com/hanium/android/maeumi/view/guide/Guide.java
@@ -1,20 +1,44 @@
 package com.hanium.android.maeumi.view.guide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hanium.android.maeumi.R;
 
 public class Guide extends AppCompatActivity {
 
+    int i = 0;
+    ImageView imageView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
-    }
 
+        Button button = (Button)findViewById(R.id.guideButton);
+        imageView = (ImageView)findViewById(R.id.guide1ImageView);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                i = 1 -i;
+
+                if(i == 0){
+                    imageView.setImageResource(R.drawable.guide1);
+                }
+                else{
+                    imageView.setImageResource(R.drawable.guide2);
+                }
+            }
+        });
+
+
+    }
     public void goToBack(View view){
         finish();
     }
