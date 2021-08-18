c384c823f5873b297205581f05d72d57f5306401
eunhye <71172389+eunhye0606@users.noreply.github.com>
2021-08-18 19:47:48 +0900

    이용안내 btn event

--- app/src/main/java/com/hanium/android/maeumi/view/guide/Guide.java
+++ app/src/main/java/com/hanium/android/maeumi/view/guide/Guide.java
@@ -19,24 +19,25 @@ public class Guide extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
 
-        Button button = (Button)findViewById(R.id.guideButton);
+        Button button =(Button) findViewById(R.id.guideButton);
         imageView = (ImageView)findViewById(R.id.guide1ImageView);
 
-        button.setOnClickListener(new View.OnClickListener(){
-
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                i = 1 -i;
-
-                if(i == 0){
-                    imageView.setImageResource(R.drawable.guide1);
-                }
-                else{
-                    imageView.setImageResource(R.drawable.guide2);
+            public void onClick(View v) {
+                for(int i =0;i<=18;i++){
+                    if (i == 0){
+                        imageView.setImageResource(R.drawable.guide1);
+                    }
+                    else if (i == 1){
+                        imageView.setImageResource(R.drawable.guide2);
+                    }
+                    else {
+                        imageView.setImageResource(R.drawable.guide3);
+                    }
                 }
             }
         });
-
 
     }
     public void goToBack(View view){
